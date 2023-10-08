923f92d281d863dc6ae2c871da843ed0597e7c6f
Mark van Booren <markvbooren@protonmail.com>
2023-10-08 10:13:00 +0200

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+
+export default function Footer () {
+    return (
+        <div>
+            Front-End Development Assignment  - Mark van Booren/ 2021
+        </div>
+    )
+
+}
